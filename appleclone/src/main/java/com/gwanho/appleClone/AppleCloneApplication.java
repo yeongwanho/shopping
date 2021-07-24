4daa0c072a4da8134d632a9b92a861ffaf6c9b93
gwanho <goanho017@naver.com>
2021-07-24 18:26:10 +0900

    애플클론페이지 메인

---
+++ appleclone/src/main/java/com/gwanho/appleClone/AppleCloneApplication.java
@@ -0,0 +1,13 @@
+package com.gwanho.appleClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppleCloneApplication.class, args);
+    }
+
+}
---
+++ appleclone/src/test/java/com/gwanho/appleClone/LegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwanho.appleClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
