068a7ee911944ea7bcc2e7be4eefd0527752e820
gwanho <goanho017@naver.com>
2021-08-26 15:55:12 +0900

    Revert "게시판 수정"
    This reverts commit 3c6f005ae97613b74df5d7e1153bec09fb97e05b.

--- appleclone/src/main/java/com/gwanho/appleClone/mainController/MainController.java
+++
@@ -1,9 +0,0 @@
-package com.gwanho.appleClone.mainController;
-
-import org.springframework.stereotype.Controller;
-
-@Controller("/")
-public class MainController {
-
-
-}
--- node/src/pages/index.tsx
+++
@@ -1,4 +0,0 @@
-function Index(){
-    return <div>Next Index Page Test</div>;
-}
-export default Index;
